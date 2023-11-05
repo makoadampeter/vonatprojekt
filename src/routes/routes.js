ad2d8821a3f62b9b628ab9b27c9fb67fe084fbad
Kovács János András <kovacs.janos212@gmail.com>
2023-11-05 16:00:44 +0100

    Regisztráció oldal get backend.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,6 +10,9 @@ module.exports = function (app){
     });
     app.get('/profile', function (request, response, next) {
         response.render('profile');
+    });
+    app.get('/registration', function(request, response, next){
+        response.render('registration_page');
         next();
     });
 }
