1069cf16cf8fe3e058fb85219d1edf21d1cca093
Csíkos Martin Nándor <h256461@stud.u-szeged.hu>
2023-11-05 13:13:23 +0100

    hostname atirva 0.0.0.0-ra

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ app.use(function(request, response, next) {
 //A routes-ban feldolgozhatjuk a get kereseket.
 require('./routes/routes')(app);
 
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = 3000;
  
 const server = http.createServer(app);
