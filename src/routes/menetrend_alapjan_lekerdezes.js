41609bed7b66c094190139c644e7ca835487cd41
Kovács János András <kovacs.janos212@gmail.com>
2023-11-07 23:42:45 +0100

    SELECT Indulasi_ido, Erkezesi_ido

--- src/routes/menetrend_alapjan_lekerdezes.js
+++ src/routes/menetrend_alapjan_lekerdezes.js
@@ -1,7 +1,7 @@
 module.exports = function (app, mysql){
     app.post('/menetrend_alapjan_lekerdezes', function(request, response, next){
 
-        const { indulasi_hely, erkezesi_hely, indulasi_ido, erkezesi_ido } = request.body;
+        const { indulasi_hely, erkezesi_hely, kezdo_indulasi_ido, kezdo_erkezesi_ido } = request.body;
 
         let db = mysql.createConnection({
             host: '127.0.0.1',
@@ -12,6 +12,7 @@ module.exports = function (app, mysql){
 
         db.query(`SELECT line_names.name AS JaratID,
         ABS(TIMEDIFF(arriving_indulas.arrival, arriving_erkezes.arrival)) AS Menetido,
+        arriving_indulas.arrival AS Indulasi_ido, arriving_erkezes.arrival AS Erkezesi_ido,
         ABS(line_stops_indulas.stop_number - line_stops_erkezes.stop_number) AS Megallok_szama
         FROM line_names
         LEFT OUTER JOIN line_stops AS line_stops_indulas ON line_stops_indulas.line = line_names.id AND line_stops_indulas.stop = ?
@@ -19,7 +20,7 @@ module.exports = function (app, mysql){
         LEFT OUTER JOIN arriving AS arriving_indulas ON arriving_indulas.line_stop_id = line_stops_indulas.id
         LEFT OUTER JOIN arriving AS arriving_erkezes ON arriving_erkezes.line_stop_id = arriving_erkezes.id
         WHERE line_stops_indulas.line = line_stops_erkezes.line AND (arriving_indulas.arrival >= ? OR arriving_erkezes.arrival <= ?)`, 
-        [indulasi_hely, erkezesi_hely, indulasi_ido, erkezesi_ido], (error, data) => {
+        [indulasi_hely, erkezesi_hely, kezdo_indulasi_ido, kezdo_erkezesi_ido], (error, data) => {
             if(error){
                 throw error;
             }
