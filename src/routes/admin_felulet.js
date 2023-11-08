5e92a0fc4f50fe693f25ae74ab9b5a72c1395fe6
Kovács János András <h160330@stud.u-szeged.hu>
2023-11-08 15:38:46 +0100

    Új járat hozzáadása

--- src/routes/admin_felulet.js
+++ src/routes/admin_felulet.js
@@ -23,7 +23,7 @@ module.exports = function (app, mysql){
     });
     app.post('/uj_jarat_hozzaadasa', function(request, response, next){
 
-        const { JaratID, Erkezes, Megallok } = request.body;
+        const { JaratID, Megallok } = request.body;
 
         let db = mysql.createConnection({
             host: 'vonat-do-user-14988675-0.c.db.ondigitalocean.com',
@@ -35,23 +35,29 @@ module.exports = function (app, mysql){
         });
 
         db.query(`INSERT INTO line_names(name) VALUES(?);
-        INSERT INTO arriving(arrival) VALUES(?);
         SELECT id FROM line_names WHERE name LIKE ?;`, 
-        [JaratID, Erkezes, JaratID], async (error, line_names) => {
+        [JaratID, JaratID], async (error, jarat) => {
             if(error){
                 throw error;
             }
             for (let i = 0; i < Megallok.length; i++) {
-                await db.query(`INSERT INTO line_stops(line, stop) VALUES(?, ?);`, 
-                [line_names[0].id, Megallok[i]], (error, data) => {
+                await db.query(`SELECT id FROM stops WHERE stops.name LIKE ?`, 
+                [Megallok[i].megallo_neve], async (error, megallo) => {
                     if(error){
                         throw error;
                     }
-                    response.send("Success");
-                    db.end();
-                    next();
+                    await db.query(`INSERT INTO line_stops(line, stop, stop_number) VALUES(?, ?, ?);
+                    INSERT INTO arriving(arrival) VALUES(?);`, 
+                    [jarat[0].id, megallo[0].id, i, Megallok[i].erkezes], (error, data) => {
+                        if(error){
+                            throw error;
+                        }
+                    });
                 });
             }
+            response.send("Success");
+            db.end();
+            next();
         });
     });
 }
