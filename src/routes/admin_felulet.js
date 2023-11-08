0432e71adeeb2fdd504dc913fa16dc9e20bb2a14
Kovács János András <h160330@stud.u-szeged.hu>
2023-11-08 16:33:40 +0100

    Járat módosítása (megállók, érkezési idők).

--- src/routes/admin_felulet.js
+++ src/routes/admin_felulet.js
@@ -82,4 +82,42 @@ module.exports = function (app, mysql){
             next();
         });
     });
+    app.post('/jarat_modositasa', function(request, response, next){
+
+        const { JaratID, Megallok } = request.body;
+
+        let db = mysql.createConnection({
+            host: 'vonat-do-user-14988675-0.c.db.ondigitalocean.com',
+            user: 'vonat-admin',
+            password: 'AVNS_BdjUT4-cpoduHn2uKQs',
+            database: 'vonat',
+            port: 25060,
+            multipleStatements: true
+        });
+
+        db.query(`SELECT id FROM line_names WHERE name LIKE ?`, 
+        [JaratID], async (error, jarat) => {
+            if(error){
+                throw error;
+            }
+            for (let i = 0; i < Megallok.length; i++) {
+                await db.query(`SELECT id FROM stops WHERE stops.name LIKE ?`, 
+                [Megallok[i].megallo_neve], async (error, megallo) => {
+                    if(error){
+                        throw error;
+                    }
+                    await db.query(`UPDATE line_stops SET stop = ? WHERE line = ?, stop_number = ?;
+                    UPDATE arriving SET arrival = ?;`, 
+                    [megallo[0].id, jarat[0].id, i, Megallok[i].erkezes], (error, data) => {
+                        if(error){
+                            throw error;
+                        }
+                    });
+                });
+            }
+            response.send("Success");
+            db.end();
+            next();
+        });
+    });
 }
