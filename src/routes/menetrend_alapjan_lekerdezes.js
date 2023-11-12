0fd23aa08a4e5e1c0d8f232c743f940863f47ce4
Kovács János András <h160330@stud.u-szeged.hu>
2023-11-12 19:53:33 +0100

    bug fixes

--- src/routes/menetrend_alapjan_lekerdezes.js
+++ src/routes/menetrend_alapjan_lekerdezes.js
@@ -1,7 +1,7 @@
 module.exports = function (app, mysql){
     app.post('/menetrend_alapjan_lekerdezes', function(request, response, next){
 
-        const { indulasi_hely, erkezesi_hely, kezdo_indulasi_ido, kezdo_erkezesi_ido } = request.body;
+        const { indulasi_hely, erkezesi_hely, kezdo_indulasi_ido } = request.body;
 
         let db = mysql.createConnection({
             host: 'vonat-do-user-14988675-0.c.db.ondigitalocean.com',
@@ -23,8 +23,8 @@ module.exports = function (app, mysql){
         LEFT OUTER JOIN arriving AS arriving_indulas ON arriving_indulas.line_stop_id = line_stops_indulas.id
         LEFT OUTER JOIN arriving AS arriving_erkezes ON arriving_erkezes.line_stop_id = arriving_erkezes.id
         LEFT OUTER JOIN users_favouriteLines ON users_favouriteLines.line = line_names.id AND users_favouriteLines.username = ?
-        WHERE line_stops_indulas.line = line_stops_erkezes.line AND (arriving_indulas.arrival >= ? OR arriving_erkezes.arrival <= ?)`, 
-        [indulasi_hely, erkezesi_hely, kezdo_indulasi_ido, kezdo_erkezesi_ido, request.session.username], (error, data) => {
+        WHERE line_stops_indulas.line = line_stops_erkezes.line AND (arriving_indulas.arrival >= ?)`, 
+        [indulasi_hely, erkezesi_hely, request.session.username, kezdo_indulasi_ido], (error, data) => {
             if(error){
                 throw error;
             }
