215948c40f998eaeabbfcd0994f2f5d7d0d19abc
Bíró Szabolcs László <h255485@stud.u-szeged.hu>
2023-11-14 03:43:54 +0000

    megállók listázásának javítása

--- src/routes/jarat_alapjan_lekerdezes.js
+++ src/routes/jarat_alapjan_lekerdezes.js
@@ -1,5 +1,8 @@
+
+
 module.exports = function (app, mysql){
-    app.post('/jarat_megallok', function(request, response, next){
+    
+    app.post('/jarat_megallok', function(request, response){
 
         const { jarat_id } = request.body;
 
@@ -10,46 +13,33 @@ module.exports = function (app, mysql){
             database: 'vonat',
             port: 25060
         });
+        let localdb = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'vonat',
+            multipleStatements: true
+        });
 
-        db.connect();
-        db.query(`SELECT * FROM line_stops WHERE line = ?,
-        line_stops.stop AS stopID,
-        line_stops.stop_number AS stopNumber,
-        line_stops.id AS stopKey,
-		SELECT * FROM leaving WHERE line_stop_id = stopNumber,
-		leaving.departure AS stopDeparture
-		SELECT name FROM stops WHERE id = stopID AS stopName`, 
-        [stopName, stopDeparture], async (error, data) => {
-            if(error){
-                throw error;
+        localdb.connect(function(err) {
+			if (err) throw err;
+		});
+
+
+        localdb.query(`
+        SELECT * FROM line_stops WHERE line = ?;`, 
+        [jarat_id], (error, data) => {
+            if (error) {
+                alert(error);
             }
-
-            response.send(data);
-            db.end();
-            next();
-            return;
-
-            let return_value = [];
-
-            for (let i = 0; i < data.length; i++) {
-                return_value.push({
-                    "stopName": data[i].stopName,
-                    "stopDeparture": data[i].stopDeparture,
-                });
-                console.log(i);
-                if(i === data.length - 1){
-                    response.send(return_value);
-                    db.end();
-                    next();
-                }
-            }
+            else{response.send(data);
+            localdb.end();}
         });
 
     });
-    app.post('/jarat_id_from_name', function(request, response, next){
-
+    app.post('/jarat_id_from_name', function(request, response) {
         const { jarat_nev } = request.body;
-
+        
         let db = mysql.createConnection({
             host: 'vonat-do-user-14988675-0.c.db.ondigitalocean.com',
             user: 'doadmin',
@@ -57,20 +47,26 @@ module.exports = function (app, mysql){
             database: 'vonat',
             port: 25060
         });
-
-        db.connect();
-        db.query(`SELECT id FROM line_names WHERE name = ?`, 
-        [jaratID], async (error, data) => {
-            if(error){
-                throw error;
-            }
-
-            response.send(data);
-            db.end();
-            next();
-            return;
+        let localdb = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'vonat',
         });
-
+    
+        localdb.connect(function(err) {
+            if (err) throw err;
+            
+            localdb.query("SELECT * FROM line_names WHERE name LIKE ?", [`%${jarat_nev}%`], (error, data) => {
+                if (error) {
+                    alert(error);
+                }
+                else{response.send(data);
+                localdb.end();}
+            });
+        });
     });
+    
+
 
 }
