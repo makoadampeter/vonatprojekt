794a85b836ed5dbaa4de7d886ceda64ca68cb44a
Bíró Szabolcs László <h255485@stud.u-szeged.hu>
2023-11-14 09:44:42 +0000

    járat megálló - menetidő (backend)

--- src/routes/jarat_alapjan_lekerdezes.js
+++ src/routes/jarat_alapjan_lekerdezes.js
@@ -37,6 +37,41 @@ module.exports = function (app, mysql){
         });
 
     });
+    app.post('/jarat_megallo_ido', function(request, response){
+
+        const { stop_id } = request.body;
+
+        let db = mysql.createConnection({
+            host: 'vonat-do-user-14988675-0.c.db.ondigitalocean.com',
+            user: 'doadmin',
+            password: 'AVNS_qw2rI_fp_NOn4kq1u9-',
+            database: 'vonat',
+            port: 25060
+        });
+        let localdb = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'vonat',
+            multipleStatements: true
+        });
+
+        localdb.connect(function(err) {
+			if (err) throw err;
+		});
+
+
+        localdb.query(`
+        SELECT arrival FROM arriving WHERE line_stop_id = ?;`, 
+        [stop_id], (error, data) => {
+            if (error) {
+                alert(error);
+            }
+            else{response.send(data);
+            localdb.end();}
+        });
+
+    });
     app.post('/jarat_id_from_name', function(request, response) {
         const { jarat_nev } = request.body;
         
