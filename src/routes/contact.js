5129fe23317756b5eb76ed9f7275e941d762e93d
Kovács János András <h160330@stud.u-szeged.hu>
2023-11-28 20:51:49 +0100

    Email cím hozzáadása.

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -1,5 +1,11 @@
 module.exports = function (app, nodemailer){
     app.post('/send_email', function(request, response, next){
-
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+              user: 'rf.vonat@gmail.com',
+              pass: 'rfvonat&123'
+            }
+          });
     });
 }
