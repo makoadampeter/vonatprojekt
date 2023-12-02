578e7e4e62f36650c7edcde5ce343fc469e7f08e
Makó Ádám Péter <h267424@stud.u-szeged.hu>
2023-12-02 16:29:56 +0000

    Profil szerkesztése backend szkript

---
+++ src/routes/profile.js
@@ -0,0 +1,47 @@
+module.exports = function (app, mysql){
+    app.post('/profile_edit', function(req, res, next){
+        let db = mysql.createConnection({
+            host: '127.0.0.1',
+            user: 'root',
+            password: '',
+            database: 'vonat'
+        });
+
+        let inputString = req.body.password;
+
+        let modifiedString = Array.from(inputString)
+            .map(char => String.fromCharCode(char.charCodeAt(0) + '2'))
+            .join('');
+
+        let inputString2 = req.body.passwordre;
+
+        let modifiedString2 = Array.from(inputString2)
+            .map(char => String.fromCharCode(char.charCodeAt(0) + '2'))
+            .join('');
+
+
+        const password = modifiedString;
+        const passwordre = modifiedString2;
+        const firstname = req.body.firstname;
+        const surname = req.body.surname;
+        const username = req.session.username;
+        const email = req.body.email;
+        const emailre = req.body.emailre;
+
+        if(password !== passwordre || email !== emailre ||  email == "" || firstname == "" || surname == "" || password == "" || passwordre == "") {
+            res.redirect('/profile');
+        } else {
+            db.connect();
+            query = `UPDATE users SET email = ?, password = ?, firstname = ?, surname = ? WHERE username = ?`;
+            db.query(query, [email, password, firstname, surname, username], (error, results, fields) => {
+                if (error) throw error;
+            });
+            req.session.email = email;
+            req.session.firstname = firstname;
+            req.session.surname = surname;
+            req.session.password = password;
+            res.redirect('/profile');
+            db.end();
+        }
+    });
+}
