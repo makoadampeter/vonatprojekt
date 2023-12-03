44b441401570787fb54e63d4aa6d820ca5b9d17b
Makó Ádám Péter <h267424@stud.u-szeged.hu>
2023-12-03 16:06:13 +0000

    Menetrend alapján lekérdezés javítása

--- src/routes/menetrend_alapjan_lekerdezes.js
+++ src/routes/menetrend_alapjan_lekerdezes.js
@@ -1,6 +1,5 @@
 module.exports = function (app, mysql){
     app.post('/menetrend_alapjan_lekerdezes', function(request, response, next){
-
         const { indulasi_hely, erkezesi_hely, kezdo_indulasi_ido } = request.body;
         if(indulasi_hely === null || erkezesi_hely === null || kezdo_indulasi_ido === null) return;
 
@@ -21,8 +20,7 @@ module.exports = function (app, mysql){
         ABS(arriving_erkezes.arrival - arriving_indulas.arrival) AS Menetido,
         arriving_indulas.arrival AS Indulasi_ido_percben,
         arriving_erkezes.arrival AS Erkezesi_ido_percben,
-        ABS(line_stops_indulas.stop_number - line_stops_erkezes.stop_number) AS Megallok_szama,
-        users_favouriteLines.line AS saved
+        ABS(line_stops_indulas.stop_number - line_stops_erkezes.stop_number) AS Megallok_szama
         FROM line_names
         LEFT OUTER JOIN stops AS indulas_stops ON indulas_stops.name LIKE ?
         LEFT OUTER JOIN stops AS erkezes_stops ON erkezes_stops.name LIKE ?
@@ -30,9 +28,8 @@ module.exports = function (app, mysql){
         LEFT OUTER JOIN line_stops AS line_stops_erkezes ON line_stops_erkezes.line = line_names.id AND line_stops_erkezes.stop = erkezes_stops.id
         LEFT OUTER JOIN arriving AS arriving_indulas ON arriving_indulas.line_stop_id = line_stops_indulas.id
         LEFT OUTER JOIN arriving AS arriving_erkezes ON arriving_erkezes.line_stop_id = line_stops_erkezes.id
-        LEFT OUTER JOIN users_favouriteLines ON users_favouriteLines.line = line_names.id AND users_favouriteLines.username = ?
-        WHERE line_stops_indulas.line = line_stops_erkezes.line`, 
-        [indulasi_hely, erkezesi_hely, request.session.username], (error, data) => {
+        WHERE line_stops_indulas.line = line_stops_erkezes.line`,
+            [indulasi_hely, erkezesi_hely], (error, data) => {
             if(error || data.length === 0){
                 response.send(error);
                 db.end();
@@ -53,15 +50,13 @@ module.exports = function (app, mysql){
                         "Menetido": parseInt(data[i].Menetido),
                         "Indulasi_ido": h_indulas + ':' + data[i].Indulasi_ido_percben,
                         "Erkezesi_ido": h_erkezes + ':' + data[i].Erkezesi_ido_percben,
-                        "Megallok_szama": parseInt(data[i].Megallok_szama),
-                        "saved": data[i].saved
+                        "Megallok_szama": parseInt(data[i].Megallok_szama)
                     });
                 }
             }
             response.send(return_value);
             db.end();
             next();
-
         });
 
         //time tipussa
@@ -69,16 +64,14 @@ module.exports = function (app, mysql){
         db.query(`SELECT line_names.name AS JaratID,
         ABS(TIMEDIFF(arriving_indulas.arrival, arriving_erkezes.arrival)) AS Menetido,
         arriving_indulas.arrival AS Indulasi_ido, arriving_erkezes.arrival AS Erkezesi_ido,
-        ABS(line_stops_indulas.stop_number - line_stops_erkezes.stop_number) AS Megallok_szama,
-        users_favouriteLines.line AS saved
+        ABS(line_stops_indulas.stop_number - line_stops_erkezes.stop_number) AS Megallok_szama
         FROM line_names
         LEFT OUTER JOIN line_stops AS line_stops_indulas ON line_stops_indulas.line = line_names.id AND line_stops_indulas.stop = ?
         LEFT OUTER JOIN line_stops AS line_stops_erkezes ON line_stops_erkezes.line = line_names.id AND line_stops_erkezes.stop = ?
         LEFT OUTER JOIN arriving AS arriving_indulas ON arriving_indulas.line_stop_id = line_stops_indulas.id
         LEFT OUTER JOIN arriving AS arriving_erkezes ON arriving_erkezes.line_stop_id = arriving_erkezes.id
-        LEFT OUTER JOIN users_favouriteLines ON users_favouriteLines.line = line_names.id AND users_favouriteLines.username = ?
         WHERE line_stops_indulas.line = line_stops_erkezes.line AND arriving_indulas.arrival >= ?`, 
-        [indulasi_hely, erkezesi_hely, request.session.username, kezdo_indulasi_ido], (error, data) => {
+        [indulasi_hely, erkezesi_hely, kezdo_indulasi_ido], (error, data) => {
             if(error){
                                 response.send(error);
                                 db.end();
@@ -92,17 +85,16 @@ module.exports = function (app, mysql){
         */
 
     });
-    app.post('/kedvenc_jarat_hozzaadasa', function(request, response, next){
+    /*app.post('/kedvenc_jarat_hozzaadasa', function(request, response, next){
 
         const { JaratID } = request.body;
         if(request.session.username && JaratID !== null){
 
             let db = mysql.createConnection({
-                host: 'vonat-do-user-14988675-0.c.db.ondigitalocean.com',
-                user: 'doadmin',
-                password: 'AVNS_qw2rI_fp_NOn4kq1u9-',
-                database: 'vonat',
-                port: 25060
+                host: '127.0.0.1',
+                user: 'root',
+                password: '',
+                database: 'vonat'
             });
             
             db.connect();
@@ -114,7 +106,7 @@ module.exports = function (app, mysql){
                     next();
                     return;
                 }
-                db.query(`INSERT INTO users_favouriteLines(username, line) VALUES(?, ?)`, 
+                db.query(`INSERT INTO users_favouritelines(username, line) VALUES(?, ?)`, 
                 [request.session.username, jarat[0].id], (error) => {
                     if(error){
                         response.send(error);
@@ -136,11 +128,10 @@ module.exports = function (app, mysql){
         const { JaratID } = request.body;
         if(request.session.username && JaratID !== null){
             let db = mysql.createConnection({
-                host: 'vonat-do-user-14988675-0.c.db.ondigitalocean.com',
-                user: 'doadmin',
-                password: 'AVNS_qw2rI_fp_NOn4kq1u9-',
-                database: 'vonat',
-                port: 25060
+                host: '127.0.0.1',
+                user: 'root',
+                password: '',
+                database: 'vonat'
             });
             
             db.connect();
@@ -152,7 +143,7 @@ module.exports = function (app, mysql){
                     next();
                     return;
                 }
-                db.query(`DELETE FROM users_favouriteLines WHERE username LIKE ? AND line = ?`, 
+                db.query(`DELETE FROM users_favouritelines WHERE username LIKE ? AND line = ?`, 
                 [request.session.username, jarat[0].id], (error) => {
                     if(error){
                         response.send(error);
@@ -173,15 +164,14 @@ module.exports = function (app, mysql){
 
         if(request.session.username){
             let db = mysql.createConnection({
-                host: 'vonat-do-user-14988675-0.c.db.ondigitalocean.com',
-                user: 'doadmin',
-                password: 'AVNS_qw2rI_fp_NOn4kq1u9-',
-                database: 'vonat',
-                port: 25060
+                host: '127.0.0.1',
+                user: 'root',
+                password: '',
+                database: 'vonat'
             });
             
             db.connect();
-            db.query(`SELECT * FROM users_favouriteLines WHERE username LIKE ?`, 
+            db.query(`SELECT * FROM users_favouritelines WHERE username LIKE ?`, 
             [request.session.username], (error, kedvenc_jaratok) => {
                 if(error || kedvenc_jaratok.length === 0){
                     response.send(error);
@@ -196,5 +186,5 @@ module.exports = function (app, mysql){
         }else{
             response.redirect('/');
         }
-    });
+    });*/
 }
