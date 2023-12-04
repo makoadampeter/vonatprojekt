56011ceb497a2c91162beed872e8cabedc7956f7
Csíkos Martin Nándor <h256461@stud.u-szeged.hu>
2023-12-04 18:15:54 +0100

    bugfix

--- src/routes/reg.js
+++ src/routes/reg.js
@@ -44,7 +44,7 @@ module.exports = function (app, mysql){
                         if (error) throw error;
         
                         if (results.length > 0) {
-                            res.redirect('/login');
+                            res.render('/login');
                         }
                     });
                     query = `INSERT INTO users (username, email, password, firstname, surname) VALUES (?, ?, ?, ?, ?)`;
